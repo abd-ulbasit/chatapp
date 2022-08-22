1f2413934d92eec400e800d70bd45327432f724c
Abdul Basit <basit01035@gmail.com>
2022-08-22 21:59:08 +0500

    Made the bottom of the  chatbox

--- client/src/components/ChatBox/ChatBottom.tsx
+++ client/src/components/ChatBox/ChatBottom.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import Button from '../UI/Button'
 import classes from "./ChatBottom.module.css"
 const ChatBottom = () => {
+    const [message, setMessage] = useState('')
+    const handlesendMessage = (e: React.FormEvent<HTMLFormElement> | undefined) => {
+        e?.preventDefault()
+        // console.log(message);
+        axios.patch("http://localhost:3000/updatechat", {
+
+        })
+
+    }
     return (
-        <div className={classes.main}>
-            bottom
-        </div>
+        <form className={classes.main} onSubmit={handlesendMessage} >
+            <input type="text" onChange={(e) => { return setMessage(e.target.value) }} value={message} />
+            <button>Send message</button>
+        </form>
     )
 }
 
-export default ChatBottom
+export default ChatBottom;
+
+// {
+//     "username": "Basit",
+//     "chatmate": "usman",
+//     "sendername": "Basit",
+//     "message": "HEllo G000000000",
+//     "timestamp": "2022-08-21T05:10:49.059Z",
+//     "delivered": true,
+//     "deliverTime": "2022-08-21T05:10:49.059Z",
+//     "read": true,
+//     "readTime": "2022-08-21T05:10:49.059Z"
+//   }
