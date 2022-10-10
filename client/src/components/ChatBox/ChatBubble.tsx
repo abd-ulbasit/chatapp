9bbf77abbbe9011880e2613c4ea4ce1f0eacccde
abd-ulbasit <basit01035@gmail.com>
2022-10-10 18:47:43 +0500

    fixed chat bubble direction error

--- client/src/components/ChatBox/ChatBubble.tsx
+++ client/src/components/ChatBox/ChatBubble.tsx
@@ -1,8 +1,9 @@
-import React, { FC, LegacyRef, MutableRefObject, useRef } from 'react'
+import React, { FC, LegacyRef, MutableRefObject, useContext, useRef } from 'react'
+import { AuthContext } from '../../contexts/AuthContext';
 import { ChatMessageType } from '../../Models/Models'
-const username = "Basit"
 const ChatBubble: FC<{ chat: ChatMessageType | undefined }> = ({ chat }) => {
     const divRef = React.useRef<HTMLDivElement>(null);
+    const { userName: username } = useContext(AuthContext);
     return (
 
         <div className={`block clear-both border rounded-full px-3 max-w-[80%] m-2 w-fit  ${chat!.sendername == username ? "float-right" : ""}`}
